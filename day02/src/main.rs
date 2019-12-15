b893986bc6014f39e296fb2913affe266d260eba
caio.messias <caio.messias@pm.me>
2019-12-15 18:12:46 -0300

    Extract intcode to a separate crate

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,74 +1,17 @@
 use std::fs;
+use intcode::IntcodeMachine;
 
-fn read_input(path: &str) -> Vec<usize> {
+fn read_input(path: &str) -> Vec<isize> {
     return fs::read_to_string(path)
         .expect("Failed to read input file. Place it in the root of the module.")
         .trim()
         .split(",")
-        .map(|num| num.parse::<usize>().unwrap())
+        .map(|num| num.parse::<isize>().unwrap())
         .collect();
 }
 
-struct IntcodeMachine {
-    tape: Vec<usize>,
-    position: usize,
-}
-
-impl IntcodeMachine {
-    fn new(tape: Vec<usize>) -> IntcodeMachine {
-        IntcodeMachine {tape, position: 0}
-    }
-
-    fn init(mut tape: Vec<usize>, noun: usize, verb: usize) -> IntcodeMachine {
-        tape[1] = noun;
-        tape[2] = verb;
-
-        IntcodeMachine {tape, position: 0}
-    }
-
-    fn addi(&mut self) {
-        let a = self.tape[self.position+1];
-        let b = self.tape[self.position+2];
-        let dest = self.tape[self.position+3];
-
-        self.tape[dest] = self.tape[a] + self.tape[b];
-        self.position += 4;
-    }
-
-    fn mult(&mut self) {
-        let a = self.tape[self.position+1];
-        let b = self.tape[self.position+2];
-        let dest = self.tape[self.position+3];
-
-        self.tape[dest] = self.tape[a] * self.tape[b];
-        self.position += 4;
-    }
-
-    fn run(&mut self) -> usize {
-        loop {
-            match self.tape[self.position] {
-                1 => self.addi(),
-                2 => self.mult(),
-                99 => return self.tape[0],
-                _ => panic!("unkown opcode")
-            }
-        }
-    }
-
-    fn run_with_target(&mut self, target: usize) -> usize {
-        loop {
-            match self.tape[self.position] {
-                1 => self.addi(),
-                2 => self.mult(),
-                99 => return self.tape[target],
-                _ => panic!("unkown opcode")
-            }
-        }
-    }
-}
-
 fn main() {
-    let tape: Vec<usize> = read_input("input");
+    let tape: Vec<isize> = read_input("input");
 
     // Part 1
     let mut machine = IntcodeMachine::init(tape.clone(), 12, 02);
@@ -84,50 +27,5 @@ fn main() {
                 println!("Part 2: 100 * {} + {} = {}", noun, verb, 100* noun + verb);
             }
         }
-    }
-}
-
-#[cfg(test)]
-pub mod tests {
-    use super::IntcodeMachine;
-
-    #[test]
-    fn example1() {
-        let tape: Vec<usize> = vec![1,9,10,3,2,3,11,0,99,30,40,50];
-        let mut machine = IntcodeMachine::new(tape);
-
-        assert_eq!(machine.run(), 3500)
-    }
-
-    #[test]
-    fn example2() {
-        let tape: Vec<usize> = vec![1,0,0,0,99];
-        let mut machine = IntcodeMachine::new(tape);
-
-        assert_eq!(machine.run(), 2)
-    }
-
-    #[test]
-    fn example3() {
-        let tape: Vec<usize> = vec![2,3,0,3,99];
-        let mut machine = IntcodeMachine::new(tape);
-
-        assert_eq!(machine.run_with_target(3), 6)
-    }
-
-    #[test]
-    fn example4() {
-        let tape: Vec<usize> = vec![2,4,4,5,99,0];
-        let mut machine = IntcodeMachine::new(tape);
-
-        assert_eq!(machine.run_with_target(5), 9801)
-    }
-
-    #[test]
-    fn example5() {
-        let tape: Vec<usize> = vec![1,1,1,4,99,5,6,0,99];
-        let mut machine = IntcodeMachine::new(tape);
-
-        assert_eq!(machine.run(), 30)
     }
 }
