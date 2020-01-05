4facace01889992e751f6d0334b971213c50487e
caio.messias <caio.messias@pm.me>
2020-01-05 15:59:21 -0300

    Add support for signals and change output to be a Option<isize> rather than a Vec<isize>

--- intcode/src/lib.rs
+++ intcode/src/lib.rs
@@ -1,8 +1,11 @@
+use std::collections::VecDeque;
+
 pub struct IntcodeMachine {
     tape: Vec<isize>,
     position: usize,
-    pub input: Vec<isize>,
-    pub output: Vec<isize>,
+    pub input: VecDeque<isize>,
+    pub output: Option<isize>,
+    status: MachineStatus,
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
@@ -11,16 +14,31 @@ enum ParameterMode {
     Immediate,
 }
 
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+enum MachineStatus {
+    Run,
+    Yield,
+    Halt,
+}
+
 impl IntcodeMachine {
     pub fn new(tape: Vec<isize>) -> IntcodeMachine {
-        IntcodeMachine {tape, position: 0, input: Vec::new(), output: Vec::new()}
+        IntcodeMachine {tape, position: 0, input: VecDeque::new(), output: Option::None, status: MachineStatus::Run}
     }
 
-    pub fn init(mut tape: Vec<isize>, noun: isize, verb: isize) -> IntcodeMachine {
-        tape[1] = noun;
-        tape[2] = verb;
+    pub fn with_init(mut self, noun: isize, verb: isize) -> IntcodeMachine {
+        self.tape[1] = noun;
+        self.tape[2] = verb;
+        return self;
+    }
 
-        IntcodeMachine {tape, position: 0, input: Vec::new(), output: Vec::new()}
+    pub fn with_input(mut self, input: isize) -> Self {
+        self.add_input(input);
+        return self;
+    }
+
+    pub fn add_input(&mut self, input: isize) {
+        self.input.push_back(input);
     }
 
     fn parse_mode(&self, i: isize) -> ParameterMode {
@@ -96,7 +114,7 @@ impl IntcodeMachine {
 
     fn st(&mut self) {
         let dest = self.fetch_dest();
-        let input = self.input.pop().unwrap();
+        let input = self.input.pop_front().unwrap();
 
         self.store(dest, input);
     }
@@ -105,7 +123,8 @@ impl IntcodeMachine {
         let mode = self.fetch1mode();
         let output = self.fetch_arg(mode);
 
-        self.output.push(output);
+        self.output = Some(output);
+        self.status = MachineStatus::Yield;
     }
 
     fn jnz(&mut self) {
@@ -148,11 +167,21 @@ impl IntcodeMachine {
         self.store(dest, result);
     }
 
+    fn halt(&mut self) {
+        self.status = MachineStatus::Halt;
+    }
+
+    pub fn is_halted(&self) -> bool {
+        return self.status == MachineStatus::Halt;
+    }
+
     pub fn run(&mut self) -> isize {
         return self.run_for_target(0);
     }
 
     pub fn run_for_target(&mut self, target: usize) -> isize {
+        self.status = MachineStatus::Run;
+
         loop {
             let opcode = self.tape[self.position] % 100;
             match opcode {
@@ -164,10 +193,18 @@ impl IntcodeMachine {
                 6 => self.jz(),
                 7 => self.tlt(),
                 8 => self.teq(),
-                99 => return self.tape[target] as isize,
+                99 => self.halt(),
                 _ => panic!("unkown opcode")
             }
             self.position += 1;
+
+            if self.status == MachineStatus::Halt {
+                return self.tape[target] as isize;
+            }
+
+            if self.status == MachineStatus::Yield {
+                return self.output.unwrap()
+            }
         }
     }
 }
@@ -227,131 +264,131 @@ mod tests {
     #[test]
     fn test_input_output() {
         let tape: Vec<isize> = vec![3, 0, 4, 0, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![1234];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(1234);
 
         machine.run();
-        assert_eq!(machine.output, vec![1234]);
+        assert_eq!(machine.output, Some(1234));
     }
 
     #[test]
     fn test_eq8_position() {
         let tape: Vec<isize> = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![8];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(8);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
     fn test_neq8_position() {
         let tape: Vec<isize> = vec![3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![5];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(5);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_lt8_position() {
         let tape: Vec<isize> = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![5];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(5);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
     fn test_nlt8_position() {
         let tape: Vec<isize> = vec![3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![80];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(80);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_eq8_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![8];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(8);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
     fn test_neq8_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1108, -1, 8, 3, 4, 3, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![9];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(9);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_lt8_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![5];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(5);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
     fn test_nlt8_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1107, -1, 8, 3, 4, 3, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![9];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(9);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_jump0_position() {
         let tape: Vec<isize> = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![0];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(0);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_jump1_position() {
         let tape: Vec<isize> = vec![3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![999];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(999);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
     fn test_jump0_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![0];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(0);
 
         machine.run();
-        assert_eq!(machine.output, vec![0]);
+        assert_eq!(machine.output, Some(0));
     }
 
     #[test]
     fn test_jump1_immediate() {
         let tape: Vec<isize> = vec![3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![999];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(999);
 
         machine.run();
-        assert_eq!(machine.output, vec![1]);
+        assert_eq!(machine.output, Some(1));
     }
 
     #[test]
@@ -359,11 +396,11 @@ mod tests {
         let tape: Vec<isize> = vec![3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
                                     1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
                                     999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![7];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(7);
 
         machine.run();
-        assert_eq!(machine.output, vec![999]);
+        assert_eq!(machine.output, Some(999));
     }
 
     #[test]
@@ -371,11 +408,11 @@ mod tests {
         let tape: Vec<isize> = vec![3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
                                     1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
                                     999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![8];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(8);
 
         machine.run();
-        assert_eq!(machine.output, vec![1000]);
+        assert_eq!(machine.output, Some(1000));
     }
 
     #[test]
@@ -383,10 +420,10 @@ mod tests {
         let tape: Vec<isize> = vec![3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
                                     1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
                                     999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99];
-        let mut machine = IntcodeMachine::new(tape);
-        machine.input = vec![9];
+        let mut machine = IntcodeMachine::new(tape)
+            .with_input(9);
 
         machine.run();
-        assert_eq!(machine.output, vec![1001]);
+        assert_eq!(machine.output, Some(1001));
     }
 }
