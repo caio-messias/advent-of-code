f47104f4dc36e47486c7e44b4c509860f9b8e82c
caio.messias <caio.messias@pm.me>
2020-01-19 18:43:10 -0300

    Add solution for day 9

--- day09/src/main.rs
+++ day09/src/main.rs
@@ -1,3 +1,22 @@
+use intcode::IntcodeMachine;
+use std::fs;
+
+fn read_input() -> Vec<i64> {
+    return fs::read_to_string("input")
+        .expect("Failed to read input file. Place it in the root of the module.")
+        .trim()
+        .split(",")
+        .map(|num| num.parse::<i64>().unwrap())
+        .collect();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let tape = read_input();
+
+    let mut machine = IntcodeMachine::new(tape.clone())
+        .with_input(2);
+
+    machine.run();
+    println!("Part 2: {}", machine.get_output().unwrap());
+
 }
